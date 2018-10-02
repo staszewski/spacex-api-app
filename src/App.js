53318c237e18ea8d15fead5f83a89d418d9f1cf6
KamilStaszewski <kamilstaszewski95@gmail.com>
2018-10-02 19:21:57 +0200

    stuff

--- src/App.js
+++ src/App.js
@@ -1,30 +1,14 @@
-import React, { Component } from 'react';
-import { StyleSheet, css } from 'aphrodite';
-import store from './store/store';
-import RepoSearch from './Components/RepoSearch'
-
+import React, { Component } from "react";
+import Fetchdata from './components/Fetchdata';
 
 class App extends Component {
   render() {
     return (
-      <div className={css(styles.app)}>
-        <RepoSearch store={store}/>
+      <div>
+        <Fetchdata />
       </div>
     );
   }
 }
 
-const styles = StyleSheet.create({
-  app: {
-    height: `100%`,
-    background: `#fff url(https://svgshare.com/i/8Gt.svg)`,
-    backgroundSize: 'cover',
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center'
-  }
-});
-
-
-
-export default App; 
+export default App;
---
+++ src/constants/ActionTypes.js
@@ -0,0 +1,13 @@
+// ActionTypes are defined in constants.
+
+// They are imported in Actions and Reducers.
+// This prevents errors if they are misspelled over here.
+
+/* eslint-disable */
+
+
+export const FETCH_DATA = "FETCH_DATA";
+export const UPDATE_INPUT = "UPDATE_INPUT";
+export const SHOW_MODAL = "SHOW_MODAL";
+export const CLOSE_MODAL = "CLOSE_MODAL";
+/* eslint-enable */
